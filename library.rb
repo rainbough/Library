d59e662c5773708afa898383a0d0356bb56ea0d0
Rainbough Phillips <rainbough@gmail.com>
2013-09-06 11:16:36 -0500

    initial commit

---
+++ library.rb
@@ -0,0 +1,59 @@
+#The initialize method requests title, author, and desciption of the book
+#then stores that info
+#
+#
+class Book
+  def initialize
+	puts "What is the title of the book?"
+	@title = gets.chomp
+	@title_sym = @title.downcase.tr(" ", "_").to_sym
+	puts "Who is the author of the book?"
+	@author = gets.chomp
+	@author_sym = @author.downcase.tr(" ", "_").to_sym
+	puts "Please enter a short description of the book:"
+	@description = gets.chomp
+	@status = :available
+  end
+#This method returns the saved information on a book.
+#It does not take an argument.
+# 
+#
+  def info
+  	puts "Title: #{@title}"
+  	puts "Author: #{@author}"
+  	puts "Description: #{@description}"
+  	puts "Status: #{@available}"
+  end
+#Checkout method
+#
+#
+#
+
+end
+
+
+puts "Welcome to the Makersquare Library."
+puts "What would you like to do?"
+puts "Commands:"
+puts "add -Add books to the library"
+=begin
+puts "list av - see a list of available books in the library"
+puts "list all - see a list of all books in the library"
+puts "list ov - see a list of overdue books in the library"
+=end
+command = gets.chomp.downcase
+
+case command
+  when "add"
+    currentbook = Book.new()
+    currentbook.info
+=begin 
+  when "list av"
+
+  when "list all"
+
+  when  "list ov"
+=end
+  else 
+  	puts "Response not understood."
+  end
